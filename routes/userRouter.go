051406bb1013ed9f86a1d587d6c98feb304f276b
gurpinder009 <gurpinders1511@gmail.com>
2025-01-06 11:48:42 -0800

    before adding gorm and mysql driver

--- routes/userRouter.go
+++ routes/userRouter.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"fmt"
 	"go_rest_api/controllers"
 
 	"github.com/gin-gonic/gin"
@@ -9,8 +10,18 @@ import (
 func RegisterUserRoutes(router *gin.Engine) {
 	userRoutes := router.Group("/users")
 	{
-		userRoutes.GET("/", controllers.GetUsers)
+		userRoutes.GET("/", middleWareFunc, middleWareFunc2, controllers.GetUsers)
 		userRoutes.GET("/:id", controllers.GetUser)
 		userRoutes.POST("/", controllers.PostUsers)
 	}
+}
+
+func middleWareFunc(ctx *gin.Context) {
+	fmt.Println("adsfasdf")
+	// ctx.Abort()
+	ctx.Next()
+}
+
+func middleWareFunc2(ctx *gin.Context) {
+	fmt.Println("mmidleware2 ")
 }
