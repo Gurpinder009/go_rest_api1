55fb2947c71e0b08da05acbe0ecae01c803cdf34
gurpinder009 <gurpinders1511@gmail.com>
2025-01-06 13:19:12 -0800

    gorm connection done and basic queries working

--- main.go
+++ main.go
@@ -1,13 +1,35 @@
 package main
 
 import (
+	"fmt"
 	"go_rest_api/routes"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+
+	"gorm.io/gorm"
 )
+
+type Student struct {
+	gorm.Model
+	Name       string
+	Rollno     uint
+	Course     string
+	Department string
+}
 
 func main() {
 	router := gin.Default()
+	dsn := "gurpinder:password@123@tcp(localhost:3306)/go_rest_api1?charset=utf8mb4"
+	db, err := gorm.Open(mysql.Open(dsn))
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	student := Student{}
+	db.AutoMigrate(&student)
+	db.Create(&Student{Name: "gurpinder", Rollno: 123, Course: "software development", Department: "Computer"})
+
 	routes.RegisterUserRoutes(router)
 	routes.RegisterPetRoutes(router)
 	router.Run("localhost:8000")
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 # basic go rest api
+
+## now we are moving towards mysql and gorm topics in golang to conquror databases in golang
 ## simple_rest.go contains a simple rest application that don't need to scale alot 
 ## remaining application contains a rest application using mvc without go_routines and complexity
